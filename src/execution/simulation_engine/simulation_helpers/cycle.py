40b37e21916b6618853bebc0e6e04c8ea99a12ad
carloj123 <carlo.laitano@acad.pucrs.br>
2020-05-26 16:59:07 -0300

    codesmells fix

--- src/execution/simulation_engine/simulation_helpers/cycle.py
+++ src/execution/simulation_engine/simulation_helpers/cycle.py
@@ -908,8 +908,7 @@ class Cycle:
                 if len(parameters) == 1:
                     match = None
                     for sub_token, sub_action, sub_param in special_action_tokens:
-                        if len(sub_param) == 1:
-                            if sub_token == parameters[0] and sub_action == 'deliverPhysical' and sub_param[2] == token:
+                        if len(sub_param) == 1 and sub_token == parameters[0] and sub_action == 'deliverPhysical' and sub_param[2] == token:
                                 match = [sub_token, sub_action, sub_param]
                                 break
 
